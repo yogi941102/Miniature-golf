f00a81dbfca4690fc01dee7137177bea12d9ed9f
jamiealoxf <yogi941102@users.noreply.github.com>
2017-07-17 18:37:32 +0800

    jamie 18.37

---
+++ MiniatureGolf/Assets/Scripts/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+    public GameObject[] FishPrefab;
+    float time = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("hahaha");
+        time += Time.deltaTime;
+        if (time >=2)
+        {
+            Debug.Log("hahaha");
+            Instantiate(FishPrefab[Random.Range(0, FishPrefab.Length)]);
+            time = 0;
+        }
+	}
+}
